6a13cad1ddf5bdc9f721421036c0f54b0d8e179d
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2022-07-07 09:57:44 -0400

    Export Client type

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,32 +2,32 @@ package client
 
 import "github.com/marcusmonteirodesouza/go-microservices-realworld-example-app-client/internal/users"
 
-type client struct {
+type Client struct {
 	Users users.UsersClient
 }
 
-func NewClient() client {
+func NewClient() Client {
 	baseURL := "https://realworld-example-app-api-api-gateway-4os2xo04.uc.gateway.dev/api"
 
 	usersClient := users.NewUsersClient(baseURL)
 
-	return client{
+	return Client{
 		Users: usersClient,
 	}
 }
 
-func NewClientWithBaseUrl(baseURL string) client {
+func NewClientWithBaseUrl(baseURL string) Client {
 	usersClient := users.NewUsersClient(baseURL)
 
-	return client{
+	return Client{
 		Users: usersClient,
 	}
 }
 
-func NewClientWithBaseUrlAndToken(baseURL string, token string) client {
+func NewClientWithBaseUrlAndToken(baseURL string, token string) Client {
 	usersClient := users.NewUsersClientWithToken(baseURL, token)
 
-	return client{
+	return Client{
 		Users: usersClient,
 	}
 }
