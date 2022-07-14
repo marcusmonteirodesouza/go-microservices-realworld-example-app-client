bacf29d017153547e824caa561a84502335627d3
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2022-07-13 21:24:42 -0400

    Add UnfollowUser method

---
+++ test/profiles/unfollow_user_test.go
@@ -0,0 +1,58 @@
+package test_users
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/bxcodec/faker/v3"
+	"github.com/marcusmonteirodesouza/go-microservices-realworld-example-app-client/pkg/client"
+	"github.com/marcusmonteirodesouza/go-microservices-realworld-example-app-client/test/utils"
+)
+
+func TestGivenValidRequestWhenUnfollowUserShouldReturnProfile(t *testing.T) {
+	followeeUsername := fmt.Sprintf("%s%s", utils.TestPrefix, faker.Username())
+	followeeEmail := fmt.Sprintf("%s%s", utils.TestPrefix, faker.Email())
+	followeePassword := faker.Password()
+
+	followeeClient := client.NewClient()
+
+	_, err := followeeClient.Users.RegisterUser(context.Background(), followeeUsername, followeeEmail, followeePassword)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	followerUsername := fmt.Sprintf("%s%s", utils.TestPrefix, faker.Username())
+	followerEmail := fmt.Sprintf("%s%s", utils.TestPrefix, faker.Email())
+	followerPassword := faker.Password()
+
+	followerClient := client.NewClient()
+
+	_, err = followerClient.Users.RegisterUser(context.Background(), followerUsername, followerEmail, followerPassword)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = followerClient.Login(followerEmail, followerPassword)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	profile, err := followerClient.Profiles.FollowUser(context.Background(), followeeUsername)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	profile, err = followerClient.Profiles.UnfollowUser(context.Background(), followeeUsername)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if profile.Profile.Username != followeeUsername {
+		t.Fatalf("got %s, want %s", profile.Profile.Username, followeeUsername)
+	}
+
+	if profile.Profile.Following {
+		t.Fatalf("got %t, want %t", profile.Profile.Following, false)
+	}
+}
